483231c6dca059c14ebf7e7efb8deb6975f79844
Isaac Zamora <zackfall@outlook.com>
2023-10-07 00:03:00 -0500

    feat: apply factorials, close #1

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod commands;
+mod types;
 
 use commands::{advanced_operations, operations};
 
@@ -9,15 +10,16 @@ fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             operations::divide,
+            operations::multiply,
+            operations::substract,
+            operations::sum,
             advanced_operations::expon,
+            advanced_operations::factorial,
             advanced_operations::ln,
             advanced_operations::log,
             advanced_operations::log_b,
-            operations::multiply,
             advanced_operations::root,
-            advanced_operations::sqrt,
-            operations::substract,
-            operations::sum
+            advanced_operations::sqrt
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
